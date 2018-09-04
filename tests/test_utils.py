e17089eff557d2da95b49e4aed1f42dd075d2cb1
Theo Alexiou <theofilosalexiou@gmail.com>
2018-09-04 21:39:22 +0200

    Added create reporter logger util

---
+++ tests/test_utils.py
@@ -0,0 +1,14 @@
+from unittest.mock import patch
+
+from .context import create_reporter
+
+
+class TestUtils(object):
+    """
+    Test the utils module
+    """
+
+    @patch('logging.Logger.addHandler')
+    def test_create_reporter(self, mock_process):
+        create_reporter()
+        assert mock_process.called
