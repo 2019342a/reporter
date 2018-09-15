ad3232fa37cd12440ffc5cb6a75e6100bcf0fdf6
Theo Alexiou <theofilosalexiou@gmail.com>
2018-09-15 16:32:44 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='reporter',
+    version='1.0.0',
+    description='Lightweight logger decorators for python',
+    author='Theofilos Alexiou',
+    author_email='theofilosalexiou@gmail.com',
+    url='https://github.com/2019342a/reporter',
+    packages=['reporter'],
+)
